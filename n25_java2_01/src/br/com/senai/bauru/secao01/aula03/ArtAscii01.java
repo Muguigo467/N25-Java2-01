67e9c57b14fda10a2ca76ad3c0ebef5021708025
Murilo <muri.ferro@outlook.com>
2025-07-30 21:51:46 -0300

    65001

---
+++ n25_java2_01/src/br/com/senai/bauru/secao01/aula03/ArtAscii01.java
@@ -0,0 +1,17 @@
+package br.com.senai.bauru.secao01.aula03;
+
+public class ArtAscii01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    	System.out.println("  #######  ");
+        System.out.println(" #########  ");
+        System.out.println("## #  # ##  ");
+        System.out.println("#  #  #  #  ");
+        System.out.println("# #    # #  ");
+        System.out.println("#  ####  #  ");
+        System.out.println(" ####### #  ");
+        System.out.println("  #######   ");   
+	}
+
+}
---
+++ n25_java2_01/src/br/com/senai/bauru/secao01/aula03/module-info.java
@@ -0,0 +1,2 @@
+package br.com.senai.bauru.secao01.aula03;
+
