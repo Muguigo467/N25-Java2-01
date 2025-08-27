7ea9d18127d3bc6be737da8a393642b6f919b64a
Murilo Ferro <muri.ferro@outlook.com>
2025-08-26 21:45:17 -0300

    65001

---
+++ n25_java2_01/projeto_banco_de_dados/src/br/com/senaisp/bauru/murilo/classes/Produto.java
@@ -0,0 +1,21 @@
+package br.com.senaisp.bauru.murilo.classes;
+
+import java.sql.SQLException;
+
+import br.com.senaisp.bauru.richard.classes.ConectorBancoDados;
+
+public class Produto {
+	private int id;
+	private String descricao;
+	private double saldo;
+	private double preco;
+	private ConectorBancoDados conn;
+	//Constructor
+	public Produto(String descricao, double saldo,
+					double preco) throws SQLException {
+		this.descricao = descricao;
+		this.saldo = saldo;
+		this.preco = preco;
+		conn = ConectorBancoDados.getInstancia();
+	}
+}
